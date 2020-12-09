63875cf257bd7fe00c89586eba0ce17d9d2bf935
Domenik Irrgang <domenikirrgang@gmail.com>
2020-12-09 15:28:05 +0100

    day2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,52 @@
 import * as fs from "fs"
 
+interface DatabaseEntry {
+    password: string
+    passwordPolicy: PasswordPolicy
+}
+
+interface PasswordPolicy {
+    minimumAmount: number
+    maximumAmount: number
+    letter: string
+}
+
+function isPasswordValid(password: string, passwordWordPolicy: PasswordPolicy): boolean {
+    return (password.charAt(passwordWordPolicy.minimumAmount - 1) == passwordWordPolicy.letter ||
+        password.charAt(passwordWordPolicy.maximumAmount - 1) == passwordWordPolicy.letter) &&
+        password.charAt(passwordWordPolicy.minimumAmount - 1) != password.charAt(passwordWordPolicy.maximumAmount - 1)
+}
+
+function countValidPasswords(database: DatabaseEntry[]): number {
+    let counter = 0
+    for (let databaseEntry of database) {
+        if (isPasswordValid(databaseEntry.password, databaseEntry.passwordPolicy)) {
+            counter++
+        }
+    }
+    return counter
+}
+
+function parseDatabaseFile(path: string): DatabaseEntry[] {
+    let database = []
+    const lines = fs.readFileSync("./input2").toString().split("\n")
+    for (let line of lines) {
+        let content = line.split(" ")
+        database.push({
+            password: content[2],
+            passwordPolicy: {
+                minimumAmount: content[0].split("-")[0],
+                maximumAmount: content[0].split("-")[1],
+                letter: content[1].replace(":", ""),
+            }
+        })
+    }
+    return database
+}
+
+console.log(countValidPasswords(parseDatabaseFile("./input2")))
+
+/*
 function getEntries(searchedNumber: number, numbers: number[]): object {
     for (let numberOne of numbers) {
         for (let numberTwo of numbers) {
@@ -31,3 +78,4 @@ for (let line of lines) {
 }
 let result = getEntriesWithThreeNumbers(2020, numbers)
 console.log(result["numberOne"] * result["numberTwo"] * result["numberThree"])
+*/
